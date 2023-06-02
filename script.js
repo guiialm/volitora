6c0963f84b3ca72b3fd37c66ae9cc0ee4b1c4ef1
guilherme <guguiejo@gmail.com>
2023-06-02 11:45:27 -0300

    teste

---
+++ script.js
@@ -0,0 +1,15 @@
+function clickMenu() {
+if (nav.itens.style.display == 'block') {
+    nav.itens.style.display == 'none'
+} else {
+    itens.display.style == 'block'
+}
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        nav.itens.style.display = 'block'
+    } else {
+        nav.itens.style.display = 'none'
+    }
+}
